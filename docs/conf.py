87803aafa8418d3c7a331a1ad8fbc59c556f0ec4
nolte <nolte07@googlemail.com>
2019-01-05 12:59:38 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinxcontrib.plantuml',
+    'm2r',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -22,7 +23,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
