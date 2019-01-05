044a0be6b6d693cffd84088cba039b85df0112e8
nolte <nolte07@googlemail.com>
2019-01-05 23:14:06 +0100

    fix script

--- ci-scripts/start_release.sh
+++ ci-scripts/start_release.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-export currentVersion=$(bumpversion --allow-dirty --dry-run --list release | grep new_version | sed -r s,"^.*=",,)
-echo "Starting Release"
-git checkout -b release/v$currentVersion-pre
-git checkout develop
 git commit --allow-empty -m "[GradeUP] grade up the branch"
-git push origin
+git push origin develop
